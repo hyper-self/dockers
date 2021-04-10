2b297acf3c2dcc782358f72739c07e361322de44
cuidongjian <cuidongjian@ata.net.cn>
2021-04-10 19:22:45 +0800

    增加app-code

--- skywalking/php-7.3-fpm-alpine/docker-entrypoint.sh
+++ skywalking/php-7.3-fpm-alpine/docker-entrypoint.sh
@@ -3,11 +3,17 @@
 set -e
 
 grpc=$SW_OAP_ADDRESS
+app_code=$APP_CODE;
 
 if [ ! $grpc ]; then
     grpc="127.0.0.1:11800"
 fi
 
+if [ ! $app_code ]; then
+    app_code="MyProjectName"
+fi
+
 sed -i "s/127.0.0.1:11800/$grpc/g" $PHP_INI_DIR/conf.d/ext-skywalking.ini
+sed -i "s/MyProjectName/$app_code/g" $PHP_INI_DIR/conf.d/ext-skywalking.ini
 
 exec "$@"
